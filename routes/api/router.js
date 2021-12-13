9f15b6efe6db63ed4f279c4aa9b7a07e84829215
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2021-12-13 16:21:13 +0100

    GET method for fetching  the chatbot messages

--- routes/api/router.js
+++ routes/api/router.js
@@ -1,6 +1,7 @@
 // Load the routers for each resource of the api
 const testQuestionsRouter = require('./endpoints/test-questions');
-const resultsRouter       =  require('./endpoints/results');
+const chabotQuestionsRouter = require('./endpoints/chabot-messages');
+const resultsRouter = require('./endpoints/results');
 
 const { Router } = require("express");
 
@@ -11,6 +12,7 @@ const router = Router();
 
 // Mount the routes here
 router.use('/test-questions',testQuestionsRouter);
+router.use('/chatbot-messages',chabotQuestionsRouter);
 router.use('/results',resultsRouter);
 
 module.exports = router;
