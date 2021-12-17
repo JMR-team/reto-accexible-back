c9233d9331bdacf79fd6cf47bbc56c635ae0e4e9
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2021-12-17 16:06:46 +0100

    He añadido un login basado en JWT al backend

--- routes/auth/router.js
+++ routes/auth/router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
 // Import the login and signup routers
-
+const loginRouter  = require('./endpoints/login'); 
 const signupRouter = require('./endpoints/signup');
 
 // Mount the routes
-router.use('/signup',signupRouter)
+router.use('/login',loginRouter);
+router.use('/signup',signupRouter);
 
 module.exports = router;
