6db093e13e95dd53429cf5afeab6073a9f02c1b9
Miguel Simon <miguel.simon@gemssy.com>
2023-08-25 13:33:00 +0200

    test

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Use dot env to load the .env file
-require('dotenv').config()
+require("dotenv").config();
 
 // Load the routers
-const apiRouter = require('./routes/api/router');
-const authRouter = require('./routes/auth/router');
+const apiRouter = require("./routes/api/router");
+const authRouter = require("./routes/auth/router");
 
 // Declare and init the app instance
 const app = express();
 
 // Cors conffiguration
-app.use(cors())
+app.use(cors());
 
 // Print logs of requests in the console
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // Parse the JSON and url encoded body of the urls
 app.use(express.json());
@@ -28,29 +28,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Mount the routers in the app
-app.use('/api', apiRouter);
-app.use('/auth',authRouter);
+app.use("/api", apiRouter);
+app.use("/auth", authRouter);
 
 // Connect the app to the database. If its necessary, add indexes here
-MongoClient.connect( process.env.MONGODB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017' , function(err, client) {
-  if(err!==undefined) {
-    console.log(err);
-  } else {
-    let db = client.db('accexible');
-    // Create an index in the users collection to avoid repeated emails
-    db.collection('users').createIndex(
-      {
-        email:1,  // Name that the user choose for the bike
-      },
-      {
-        name:"unique_email", //name of the index, not the same name as above
-        unique:true,
-      }
-    )
-    // add the database to the app
-    app.locals.db = db;
+MongoClient.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  },
+  function (err, client) {
+    if (err !== undefined) {
+      console.log(err);
+    } else {
+      let db = client.db("accexible");
+      // Create an index in the users collection to avoid repeated emails
+      db.collection("users").createIndex(
+        {
+          email: 1, // Name that the user choose for the bike
+        },
+        {
+          name: "unique_email", //name of the index, not the same name as above
+          unique: true,
+        }
+      );
+      // add the database to the app
+      app.locals.db = db;
+    }
   }
-});
+);
 
 // Export the app module to be used in ./bin/www
 module.exports = app;
