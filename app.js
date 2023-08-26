1424a6dda2bf340b3daa67115019473fbcc70a01
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2023-08-26 21:01:27 +0200

    refactor

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
 // Use dot env to load the .env file
-require("dotenv").config();
+require('dotenv').config();
 
 // Load the routers
-const apiRouter = require("./routes/api/router");
-const authRouter = require("./routes/auth/router");
+const apiRouter = require('./routes/api/router');
+const authRouter = require('./routes/auth/router');
 
 // Declare and init the app instance
 const app = express();
@@ -18,7 +17,7 @@ const app = express();
 app.use(cors());
 
 // Print logs of requests in the console
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 // Parse the JSON and url encoded body of the urls
 app.use(express.json());
@@ -28,33 +27,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Mount the routers in the app
-app.use("/api", apiRouter);
-app.use("/auth", authRouter);
-
-// Connect the app to the database. If its necessary, add indexes here
-MongoClient.connect(
-  process.env.MONGODB_CONNECTION_STRING,
-  {},
-  function (err, client) {
-    if (err !== undefined) {
-      console.log(err);
-    } else {
-      let db = client.db("accexible");
-      // Create an index in the users collection to avoid repeated emails
-      db.collection("users").createIndex(
-        {
-          email: 1, // Name that the user choose for the bike
-        },
-        {
-          name: "unique_email", //name of the index, not the same name as above
-          unique: true,
-        }
-      );
-      // add the database to the app
-      app.locals.db = db;
-    }
-  }
-);
+app.use('/api', apiRouter);
+app.use('/auth', authRouter);
 
 // Export the app module to be used in ./bin/www
 module.exports = app;
